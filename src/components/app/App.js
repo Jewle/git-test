63b70e39786d5ce2d95f0cd5341560810be620c5
Artem Chizhov <artem4ik500@yandex.ru>
2021-12-30 00:08:08 +0300

    'change'

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,9 +1,12 @@
 import React from "react";
+import {useState} from "react";
 
 export default function () {
+    const [title, setTitle] = useState('Hello')
     return (
     <div className='application'>
-        <h1>Hello</h1>
+        <h1>{title}</h1>
+        <button onClick={()=>setTitle(state=>state==='Hello' ? 'Pidor' : 'Hello')}>Click me</button>
     </div>
     )
 }
